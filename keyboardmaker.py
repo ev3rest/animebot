4e8beebb688df0048297009a766364eb4c55e810
ev3rest <appleverest@gmail.com>
2020-08-17 22:56:24 +0600

    keyboard changed

--- keyboardmaker.py
+++ keyboardmaker.py
@@ -2,7 +2,7 @@ from aiogram import types
 from enums import Functions
 def normalkeyboard(command_id, post_id):
 	keyboard = types.InlineKeyboardMarkup(row_width=1)
-	row_btns = (types.InlineKeyboardButton('Download', callback_data='%s:%s'%(Functions.DOWNLOAD, post_id)),\
+	row_btns = (types.InlineKeyboardButton('Download', callback_data='%s:%s:%s'%(Functions.DOWNLOAD, command_id, post_id)),\
 				types.InlineKeyboardButton('Next', callback_data='%s:%s' %(Functions.NEXT, command_id))) #function:data
 	keyboard.row(*row_btns)
 	return keyboard
